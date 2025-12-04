5fe407fef621ac137bf5a903bdf15a5598572adf
heenauniharkhaniths <HeenaUnihar.Khan@iths.se>
2025-12-04 14:46:27 +0100

    12/4

---
+++ demo/src/test/java/kitchen/kitchenQueueTest.java
@@ -0,0 +1,16 @@
+package kitchen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class kitchenQueueTest {
+    
+    //New order appears automatically
+
+    
+
+
+
+
+}
